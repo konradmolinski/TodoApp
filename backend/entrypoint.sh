9343d7adc05f405b3bd8af78208288d34e997587
wjurkowl <wjurkowl@cisco.com>
2022-10-24 22:31:15 +0200

    livereload of app

---
+++ backend/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+set -e
+
+## Run startup command
+alembic upgrade head
+
+## Running passed command
+if [[ "$1" ]]; then
+	eval "$@"
+else
+	uvicorn todo_app.main:app --port 8000 --host 0.0.0.0 --reload
+fi
